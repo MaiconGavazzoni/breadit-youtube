1e4288b6b43125d5afddf2117d45dfec5fa80789
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2023-07-17 22:45:10 -0300

    subscribe and unsubscribe

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,4 +1,54 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
+import { buttonVariants } from "@/components/ui/Button"
+import { getAuthSession } from "@/lib/auth"
+import { db } from "@/lib/db"
+import Link from "next/link"
+import { format } from "date-fns"
+import { notFound } from "next/navigation"
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle"
+
+const Layout = async ({
+  children,
+  params: { slug },
+}: {
+  children: React.ReactNode
+  params: { slug: string }
+}) => {
+  const session = await getAuthSession()
+
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+        },
+      },
+    },
+  })
+
+  const subscription = !session?.user
+    ? undefined
+    : await db.subscription.findFirst({
+        where: {
+          subreddit: {
+            name: slug,
+          },
+          user: { id: session.user.id },
+        },
+      })
+
+  const isSubscribed = !!subscription
+
+  const memberCount = await db.subscription.count({
+    where: {
+      subreddit: {
+        name: slug,
+      },
+    },
+  })
+
+  if (!subreddit) return notFound()
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
@@ -7,6 +57,49 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           <div className="flex flex-col col-span-2 space-y-6">{children}</div>
 
           {/* info sidebar */}
+          <div className="hidden md:block overflow-hidden h-fit rouded-lg border border-gray-200 order-fit md:order-last">
+            <div className="px-6 py-4">
+              <p className="font-semibold py-3">About r/</p>
+            </div>
+            <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Created</dt>
+                <dd className="text-gray-700">
+                  <time dateTime={subreddit.createdAt.toDateString()}>
+                    {format(subreddit.createdAt, "MMMM d, yyyy")}
+                  </time>
+                </dd>
+              </div>
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Members</dt>
+                <dd className="flex items-start gap-x-2">
+                  <div className="text-gray-900">{memberCount}</div>
+                </dd>
+              </div>
+              {subreddit.creatorId === session?.user?.id ? (
+                <div className="flex justify-between gap-x-4 py-3">
+                  <dt className="text-gray-500">You created this community</dt>
+                </div>
+              ) : null}
+
+              {subreddit.creatorId !== session?.user?.id ? (
+                <SubscribeLeaveToggle
+                  isSubscribed={isSubscribed}
+                  subredditId={subreddit.id}
+                  subredditName={subreddit.name}
+                />
+              ) : null}
+              <Link
+                className={buttonVariants({
+                  variant: "outline",
+                  className: "w-full mb-6",
+                })}
+                href={`r/${slug}/submit`}
+              >
+                Create Post
+              </Link>
+            </dl>
+          </div>
         </div>
       </div>
     </div>
