c0678cc981fe481b912200d7f493c5df83d584a9
Maicon Gavazzoni <maicon@datasystemweb.com.br>
2023-07-21 17:40:02 -0300

    Editor and List Posts

--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -6,4 +6,7 @@ export const PostValidator = z.object({
     .min(3, { message: "Title must be longer than 3 characters" })
     .max(128, { message: "Title must be at least 128 characters" }),
   subredditId: z.string(),
+  content: z.any(),
 })
+
+export type PostCreationRequest = z.infer<typeof PostValidator>
